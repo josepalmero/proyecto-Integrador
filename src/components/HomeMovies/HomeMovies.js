0e562a7aad262ee2a5c73296aa52bcdab1f7df1d
Josefina Palmero <josepalmero@MacBook-Air-de-Josefina-4.local>
2024-09-16 18:02:28 -0300

    estructura components

---
+++ src/components/HomeMovies/HomeMovies.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const HomeMovies = () => {
+  return (
+    <div>HomeMovies</div>
+  )
+}
---
+++ src/components/MoviesGrid/MoviesGrid.js
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const MoviesGrid = () => {
+  return (
+    <div>MoviesGrid</div>
+  )
+}
