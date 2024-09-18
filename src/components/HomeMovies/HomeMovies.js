f6f55cbfed01680de9282b4f1672a3e1ac923b72
Serena Fabbian <serenafabbian@MacBook-Air-de-Serena.local>
2024-09-18 14:29:12 -0300

    home

--- src/components/HomeMovies/HomeMovies.js
+++ src/components/HomeMovies/HomeMovies.js
@@ -1,8 +1,46 @@
-import React from 'react'
+import React, { Component } from "react";
+import { options } from "../options"
 
-export const HomeMovies = () => {
-  return (
-    <div>HomeMovies</div>
-  )
+class HomeMovies extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      datos: " "
+    }
+  }
+
+  ////API de populares 
+  componentDidMount(){
+    fetch("https://api.themoviedb.org/3/movie/popular", options)
+      .then( response => response.json() )
+      .then( data => console.log(data) )   
+      .catch( error => console.log(error) );
+  }
+
+  render(){
+    return( 
+      <div>HomeMovies</div>
+    );
+  };
+
+  ///no estoy segura si van el el mismo lo de populares y cartelera 
+
+
+  ////API de cartelera 
+  componentDidMount(){
+    fetch("https://api.themoviedb.org/3/movie/now_playing", options)
+      .then( response => response.json() )
+      .then( data => console.log(data) )   
+      .catch( error => console.log(error) );
+  }
+
+  render(){
+    return( 
+      <div>HomeMovies</div>
+    );
+  };
+
 }
+
+export default HomeMovies
 
