cd5af8bae332a2e229d8ca26951aa1fb87598939
bdgutter <bguttercorsi@udesa.edu.ar>
2024-09-23 12:36:48 -0300

    pagina VerTodas wip

---
+++ src/pages/VerTodas.js
@@ -0,0 +1,51 @@
+import { Component } from 'react';
+import { options } from "../options"
+import HomeMovies from "../components/HomeMovies/HomeMovies";
+
+
+class VerTodas extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+          movies: [],
+          moviesFiltered: [],
+          isLoading: true,
+        }
+      }    
+
+    componentDidMount() {
+        this.setState({
+            isLoading: true
+          })
+      
+          const url = this.props.url
+      
+          fetch(url, options)
+            .then((response) => response.json())
+            .then((data) => { this.setState({ movies: data.results, moviesFiltered: data.results }) })
+            .catch((error) => console.log(error));
+      
+          this.setState({
+            isLoading: false
+          })    
+    }
+
+    handleFilterChange() {
+        this.setState({
+            moviesFiltered: movies.filter(movie => movie.title.includes(this.state.filterValue)) //movies figura como not defined(?)
+        })
+    }
+
+    render() {
+        return(
+            <div>
+
+            </div>
+        )
+            //incluir form para filtrar
+            //incluir boton ver mas
+    }
+
+}
+
+export default VerTodas;
